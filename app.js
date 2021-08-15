e8db1d060843091a7a8232ddb94560d5fe358a6d
AOB-Creator <alpamisomirbekov630@gmail.com>
2021-08-15 22:37:05 +0500

    small change

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 let user = {
     email: "",
+    username: "",
     password: ""
 }
 let tester = {
     ema:"",
+    uname:"",
     p1:"",
     p2:""
 }
@@ -26,11 +28,15 @@ if(localStorage.getItem("user")===null){
     const button = document.querySelector(".sign-up button")
 
     const email = document.querySelector("#email")
+    const username = document.querySelector("#username")
     const pass1 = document.querySelector("#password")
     const pass2 = document.querySelector("#password2")
 
     email.addEventListener("change", (e)=>{
         tester.ema=e.target.value
+    })
+    username.addEventListener("change", (e)=>{
+        tester.uname=e.target.value
     })
     pass1.addEventListener("change", (e)=>{
         tester.p1 = e.target.value
@@ -41,20 +47,29 @@ if(localStorage.getItem("user")===null){
     
     button.addEventListener("click", function(e){
         e.preventDefault()
-        const {ema, p1, p2}=tester;
+        const {ema, uname, p1, p2}=tester;
         console.log(ema, p1, p2)
-        if(email!==""){
+        if(ema!=="" && uname!==""){
         if(p1===p2 && p1!==""){
+            if(p1.length>=8){
                 user.email = ema
+                user.username = uname
                user.password=p1;
                localStorage.setItem("user", JSON.stringify(user))  
                user.email=""
+               user.username=""
                user.password=""      
                signup.classList.add("none")
-        pages.classList.remove("none")
+                pages.classList.remove("none")
+            }else{
+                alert("xavfsizlik uchun passwordta kamida 8ta simvol ishlating")
+            }
+                
+        }else{
+            alert("Passowords are not the same")
         }
     }else{
-        alert("")
+        alert("Barch katakchalar toldirilishi shart")
     }
 }
     )
@@ -74,20 +89,17 @@ if(localStorage.getItem("user")===null){
             logger.data2 = e.target.value
             
         })
-    const alter = logbtn.addEventListener("click", (e)=>{
+    logbtn.addEventListener("click", (e)=>{
             e.preventDefault()
             console.log(logger.data1, logger.data2);
         const datauser = JSON.parse(localStorage.getItem("user"))
-        const {email, password} = datauser;
+        const {username, password} = datauser;
         
         
 
-        if( logger.data1 == email && logger.data2==password){      
-    
+        if( logger.data1 == username && logger.data2==password){      
                 k=0;
-                pages.classList.remove("none")
-                
-                
+                pages.classList.remove("none")                
                 logout.addEventListener("click",  function(){
                     localStorage.removeItem("user")          
                     signup.classList.remove("none")
@@ -97,18 +109,28 @@ if(localStorage.getItem("user")===null){
                 })
                 login.classList.add("none")
             } else {
-                // k++
+                const waiting  = document.querySelector(".waiting-time")
+                const timer  = document.querySelector(".timer")
+                waiting.textContent = "you have several error"    
+                logbtn.classList.add("none")
                 
-                // setTimeout(() => {
+                let time = k*3;
+                const a=setInterval(()=>{
+                    timer.classList.remove("none")
+                    timer.textContent=time 
+                    time--
                     
-                // logbtn.removeEventListener()
-                    
-                // }, k*1000);
+
+                }, 1000)
+                setTimeout(() => {
+                    logbtn.classList.remove("none")
+                    waiting.textContent = ""    
+                    timer.classList.add("none")
+                    timer.textContent="" 
+                    clearInterval(a)
+                }, k*3000);
+                k++
                 
-                // setInterval(() => {
-                //     const waiting = document.querySelector(".waiting-time")
-                //     waiting.textContent = k;
-                // }, 1000);
                 
              }      
             }
